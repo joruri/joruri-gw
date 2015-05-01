9ca2feed3909d9c3211f6e90db63e0b689d72c1d
Yoshikazu Kaneta <kaneta@idsinc.co.jp>
2015-05-01 14:16:42 +0900

    データ移行 SSO修正

--- db/migrate_pref/20140912050150_insert_into_gw_access_controls.rb
+++ db/migrate_pref/20140912050150_insert_into_gw_access_controls.rb
@@ -3,7 +3,7 @@ class InsertIntoGwAccessControls < ActiveRecord::Migration
     execute "insert into gw_access_controls (state, user_id, path, priority) values ('enabled', 5431, '/gw/pref_only_assembly', 1)" 
     execute "insert into gw_access_controls (state, user_id, path, priority) values ('enabled', 5431, '/gw/pref_only_executives', 10)" 
     execute "insert into gw_access_controls (state, user_id, path, priority) values ('enabled', 5431, '/gw/pref_only_directors', 10)" 
-    execute "insert into gw_access_controls (state, user_id, path, priority) values ('enabled', 5470, '/_admin/gw/link_sso/redirect_to_joruri?to=mail&path=/pes', 1)" 
+    execute "insert into gw_access_controls (state, user_id, path, priority) values ('enabled', 5470, '/_admin/gw/link_sso/redirect_to_joruri?to=gw_ind&path=/pes', 1)" 
   end
   def down
   end
