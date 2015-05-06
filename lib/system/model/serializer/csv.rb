6d96a28003e9517ab408014b4c5e8cf29dbd556e
Yoshikazu Kaneta <kaneta@idsinc.co.jp>
2015-05-06 20:13:33 +0900

    csv リファクタリング

--- lib/system/model/serializer/csv.rb
+++ lib/system/model/serializer/csv.rb
@@ -4,8 +4,8 @@ module System::Model::Serializer::Csv
 
   module ClassMethods
     def to_csv(options = {}, csv_options = {})
-      headers = options.key?(:headers) ? options[:headers] : csv_headers(options)
-      columns = options.key?(:columns) ? options[:columns] : csv_columns(options)
+      headers = csv_headers(options)
+      columns = csv_columns(options)
       CSV.generate(csv_options.reverse_merge(force_quotes: true)) do |csv|
         csv << headers if headers.present?
         all.each do |item|
@@ -20,8 +20,8 @@ module System::Model::Serializer::Csv
 
     def from_csv(str, options = {}, csv_options = {})
       items = []
-      headers = options.key?(:headers) ? options[:headers] : csv_headers(options)
-      columns = options.key?(:columns) ? options[:columns] : csv_columns(options)
+      headers = csv_headers(options)
+      columns = csv_columns(options)
       CSV.parse(str, csv_options.reverse_merge(headers: true)).each do |row|
         if block_given?
           items << yield(row)
@@ -40,19 +40,27 @@ module System::Model::Serializer::Csv
       option == :default ? model_name.i18n_key : option
     end
 
+    def select_csv_columns(options = {})
+      columns = column_names.dup
+      columns.reject! {|name| name.in?(options[:except]) || name.to_sym.in?(options[:except]) } if options[:except]
+      columns.select! {|name| name.in?(options[:only]) || name.to_sym.in?(options[:only]) } if options[:only]
+      columns
+    end
+
     def csv_columns(options = {})
-      if options[:i18n_key]
+      if options.key?(:columns)
+        options[:columns]
+      elsif options[:i18n_key]
         I18n.t("csv.headers.#{csv_i18n(options[:i18n_key])}").keys.map(&:to_s)
       else
-        columns = column_names.dup
-        columns.reject! {|name| name.in?(options[:except].map(&:to_s)) } if options[:except]
-        columns.select! {|name| name.in?(options[:only].map(&:to_s)) } if options[:only]
-        columns
+        select_csv_columns(options)
       end
     end
 
     def csv_headers(options = {})
-      if options[:i18n_key]
+      if options.key?(:headers)
+        options[:headers]
+      elsif options[:i18n_key]
         I18n.t("csv.headers.#{csv_i18n(options[:i18n_key])}").values
       else
         csv_columns(options)
