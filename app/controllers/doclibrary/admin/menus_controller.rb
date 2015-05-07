fd3469c2f63719666e7aed063ffc933e134f779e
Yoshikazu Kaneta <kaneta@idsinc.co.jp>
2015-05-07 20:01:40 +0900

    議会図書室情報リンク表示

--- app/controllers/doclibrary/admin/menus_controller.rb
+++ app/controllers/doclibrary/admin/menus_controller.rb
@@ -9,9 +9,18 @@ class Doclibrary::Admin::MenusController < Gw::Controller::Admin::Base
   end
 
   def index
-    @items = Doclibrary::Control.where(state: 'public', view_hide: 1)
-      .tap {|c| break c.with_readable_role(Core.user) unless Doclibrary::Control.is_sysadm? }
-      .order(sort_no: :asc, docslast_updated_at: :desc)
-      .paginate(page: params[:page], per_page: params[:limit]).distinct
+    @items = load_items(Doclibrary::Control)
+
+    @admin_items = []
+    @admin_items = load_items(Adminlibrary::Control) if Adminlibrary::Control.table_exists?
+  end
+
+  private
+
+  def load_items(control_model)
+    items = control_model.distinct.where(state: 'public', view_hide: 1)
+    items = items.with_readable_role(Core.user) unless control_model.is_sysadm?
+    items.order(sort_no: :asc, docslast_updated_at: :desc)
+      .paginate(page: params[:page], per_page: params[:limit])
   end
 end
