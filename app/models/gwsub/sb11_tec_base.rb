8fa13ca0c73f8426ee23f8286c249cebdc379a5c
tao-k <tao@sitebridge.co.jp>
2015-09-03 14:01:50 +0900

    fix gwsub sb11
    authority check

--- app/models/gwsub/sb11_tec_base.rb
+++ app/models/gwsub/sb11_tec_base.rb
@@ -4,15 +4,9 @@ class Gwsub::Sb11TecBase < Gwsub::GwsubPref
   establish_connection :gwsub rescue nil
 
   def self.is_readable?
-    uid = Core.user.id
-    sys_admin_role  = Core.user.has_role?("_admin/admin")
-    return true if sys_admin_role==true
-    tec_admin_role  = Core.user.has_role?("tokushima/admin")
-    return true if tec_admin_role==true
-    user  = System::User.where(:id => uid).first
-    return false if user.blank?
-    return false if user.state=='disabled'
-    base_data = Gwsub::Sb11TecBase.where(:state => 1, :uid => uid).order("state, uid").first
+    return true if Core.user.has_role?("_admin/admin")
+    return true if Core.user.has_role?("tokushima/admin")
+    base_data = Gwsub::Sb11TecBase.where(:state => 1, :uid => Core.user.id).order("state, uid").first
     return true if base_data.present?
     return false
   end
