b3f2f0f223e50208bea36c796a537a129e0f6969
tao-k <tao@sitebridge.co.jp>
2015-09-03 14:45:25 +0900

    fix gwsub11

--- app/models/gwsub/sb11_tec_base.rb
+++ app/models/gwsub/sb11_tec_base.rb
@@ -4,10 +4,7 @@ class Gwsub::Sb11TecBase < Gwsub::GwsubPref
   establish_connection :gwsub rescue nil
 
   def self.is_readable?
-    return true if Core.user.has_role?("_admin/admin")
-    return true if Core.user.has_role?("tokushima/admin")
-    base_data = Gwsub::Sb11TecBase.where(:state => 1, :uid => Core.user.id).order("state, uid").first
-    return true if base_data.present?
+    return true if Core.user.has_role?("_admin/admin", "tokushima/admin") || Gwsub::Sb11TecBase.where(:state => 1, :uid => Core.user.id).exists?
     return false
   end
 
