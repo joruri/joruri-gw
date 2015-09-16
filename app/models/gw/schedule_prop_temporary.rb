af6f7f0e184481d7b953b0e3e3ede7b53dd1f81d
tao-k <tao@sitebridge.co.jp>
2015-09-16 10:09:15 +0900

    fix schedule prop
    add options

--- app/models/gw/schedule_prop_temporary.rb
+++ app/models/gw/schedule_prop_temporary.rb
@@ -7,10 +7,11 @@ class Gw::SchedulePropTemporary < Gw::Database
   scope :rentcars, ->{where(:prop_type=>"Gw::PropRentcar")}
 
   scope :check_duplication, ->(tmp_id, st_at, ed_at, prop_id,current_time) {
+    interval = AppConfig.gw.confirmation_rentcar['interval'].presence || 1
     rel = all.where.not(:tmp_id => tmp_id)
              .where(arel_table[:st_at].lteq(ed_at))
              .where(arel_table[:ed_at].gteq(st_at))
-             .where(arel_table[:created_at].gt(current_time - 5.minutes))
+             .where(arel_table[:created_at].gt(current_time - interval.minutes))
              .where(arel_table[:created_at].lteq(current_time))
              .where(:prop_id=>prop_id)
     rel
