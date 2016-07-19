b4c404c951693c0951c58c82e688229db117544d
tao-k <tao@sitebridge.co.jp>
2016-07-19 18:03:12 +0900

    add d and d upload(gwmonitor, gwcircular, gwworkflow)

--- app/controllers/gwboard/admin/ajaxuploads_controller.rb
+++ app/controllers/gwboard/admin/ajaxuploads_controller.rb
@@ -4,32 +4,41 @@ class Gwboard::Admin::AjaxuploadsController < Gw::Controller::Admin::Base
 
   def pre_dispatch
     return http_error(404) if params[:system].blank?
+    @parent_id = params[:parent_id]
     case params[:system]
+    when 'gwmonitor_base'
+      @title = Gwmonitor::Control.find(@parent_id)
     when 'gwmonitor'
-      @parent_id = params[:gwmonitor_id]
       @title = Gwmonitor::Control.find(params[:title_id])
     when 'gwcircular'
       @title = Gwcircular::Control.find(params[:title_id])
       @doc = @title.docs.find(@parent_id)
-      @parent_id = params[:gwcircular_id]
     when 'gwworkflow'
-      @parent_id = params[:gwworkflow_id]
       @title = Gwworkflow::Control.find(params[:title_id])
     else
       @title = Gwboard.control_model(params[:system]).find(params[:title_id])
-      @parent_id = params[:parent_id]
     end
   end
 
   def create
     if params[:files]
       params[:files].each do |file|
-        @item = @title.files.build(
-          file: file,
-          parent_id: @parent_id,
-          content_id: @title.upload_system,
-          db_file_id: 0
-        )
+        if params[:system] == 'gwmonitor_base'
+          @item = @title.base_files.build(
+            file: file,
+            parent_id: @parent_id,
+            content_id: @title.upload_system,
+            db_file_id: 0
+          )
+        else
+          @item = @title.files.build(
+            file: file,
+            parent_id: @parent_id,
+            content_id: @title.upload_system,
+            db_file_id: 0
+          )
+        end
+
       end
     end
 
