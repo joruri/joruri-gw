17c88becf64f6bb9b3c983448a5a3b4d3c7cec57
tao-k <tao@sitebridge.co.jp>
2016-09-29 13:08:06 +0900

    fix mailer

--- lib/system/controller/mail/smtp.rb
+++ lib/system/controller/mail/smtp.rb
@@ -1,4 +1,4 @@
-class System::Controller::Mail::Smtp < ActionMailer::Base
+class System::Controller::Mail::Smtp < DefaultMailer
   def default_mail(mail_fr, mail_to, subject, message)
     mail({
       :from    => mail_fr,
