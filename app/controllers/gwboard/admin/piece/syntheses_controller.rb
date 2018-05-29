2cdc1925328d59b612a878fec67c6a16cb2f7d14
tao-k <tao@sitebridge.co.jp>
2018-05-29 18:28:12 +0900

    bump version to 2.3.3

--- app/controllers/gwboard/admin/piece/syntheses_controller.rb
+++ app/controllers/gwboard/admin/piece/syntheses_controller.rb
@@ -3,7 +3,7 @@ class Gwboard::Admin::Piece::SynthesesController < ApplicationController
   include Gwboard::Controller::Scaffold
   include Gwboard::Controller::Authorize
   layout 'base'
-  
+
   def initialize_scaffold
     item = Gwboard::Synthesetup.new
     item.and :content_id, 0
@@ -27,13 +27,25 @@ class Gwboard::Admin::Piece::SynthesesController < ApplicationController
     when '4.days'
       @msg = '4日前から'
       @date = 4.days.ago.strftime('%Y-%m-%d')
+    when '5.days'
+      @msg = '5日前から'
+      @date = 5.days.ago
+    when '6.days'
+      @msg = '6日前から'
+      @date = 6.days.ago
+    when '7.days'
+      @msg = '一週間前から'
+      @date = 7.days.ago
+    when '14.days'
+      @msg = '2週間前から'
+      @date = 14.days.ago
     else
       @msg = '本日'
       @date = Date.yesterday.strftime('%Y-%m-%d')
     end
     @date = @date + " 00:00:00"
   end
-  
+
   def index
     index_gwbbs       if @select && @select.gwbbs_check
     index_gwfaq       if @select && @select.gwfaq_check
@@ -48,62 +60,62 @@ class Gwboard::Admin::Piece::SynthesesController < ApplicationController
     @none_dsp = false unless @doclib_docs.blank?
     @none_dsp = false unless @digitallib_docs.blank?
   end
-  
+
   def index_gwbbs
     item = Gwboard::Synthesis.new
     item.gwbbs_readable_syntheses(@date)
-    items = item.find(:all, 
+    items = item.find(:all,
       :select => 'latest_updated_at',
       :order => 'gwboard_syntheses.latest_updated_at DESC')
-    
+
     if items.length > 0
       @bbs_docs = "#{@msg} #{items.length}件の更新あり。最新記事は [ #{items[0].latest_updated_at.strftime('%Y年%m月%d日 %H時%M分').to_s} ] に更新。"
     end
   end
-  
+
   def index_gwfaq
     item = Gwboard::Synthesis.new
     item.gwfaq_readable_syntheses(@date)
-    items = item.find(:all, 
+    items = item.find(:all,
       :select => 'latest_updated_at',
       :order => 'gwboard_syntheses.latest_updated_at DESC')
-    
+
     if items.length > 0
       @faq_docs = "#{@msg} #{items.length}件の更新あり。最新記事は [ #{items[0].latest_updated_at.strftime('%Y年%m月%d日 %H時%M分').to_s} ] に更新。"
     end
   end
-  
+
   def index_gwqa
     item = Gwboard::Synthesis.new
     item.gwqa_readable_syntheses(@date)
-    items = item.find(:all, 
+    items = item.find(:all,
       :select => 'latest_updated_at',
       :order => 'gwboard_syntheses.latest_updated_at DESC')
-    
+
     if items.length > 0
       @qa_docs = "#{@msg} #{items.length}件の更新あり。最新記事は [ #{items[0].latest_updated_at.strftime('%Y年%m月%d日 %H時%M分').to_s} ] に更新。"
     end
   end
-  
+
   def index_doclib
     item = Gwboard::Synthesis.new
     item.doclibrary_readable_syntheses(@date)
-    items = item.find(:all, 
-      :select => 'latest_updated_at', 
+    items = item.find(:all,
+      :select => 'latest_updated_at',
       :order => 'gwboard_syntheses.latest_updated_at DESC')
-    
+
     if items.length > 0
       @doclib_docs = "#{@msg} #{items.length}件の更新あり。最新記事は [ #{items[0].latest_updated_at.strftime('%Y年%m月%d日 %H時%M分').to_s} ] に更新。"
     end
   end
-  
+
   def index_digitallib
     item = Gwboard::Synthesis.new
     item.digitallibrary_readable_syntheses(@date)
-    items = item.find(:all, 
+    items = item.find(:all,
       :select => 'latest_updated_at',
       :order => 'gwboard_syntheses.latest_updated_at DESC')
-    
+
     if items.length > 0
       @digitallib_docs = "#{@msg} #{items.length}件の更新あり。最新記事は [ #{items[0].latest_updated_at.strftime('%Y年%m月%d日 %H時%M分').to_s} ] に更新。"
     end
