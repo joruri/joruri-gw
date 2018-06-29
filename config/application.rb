5d1929feb17657784c232da962f41861ddf06759
tao-k <tao@sitebridge.co.jp>
2018-06-29 18:38:11 +0900

    add sanitize option

--- config/application.rb
+++ config/application.rb
@@ -51,11 +51,16 @@ module JoruriGw
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+
+    config.action_view.sanitized_allowed_tags = ActionView::Base.sanitized_allowed_tags.to_a | %w(table caption tr th td iframe)
+    config.action_view.sanitized_allowed_attributes = ActionView::Base.sanitized_allowed_attributes.to_a | %w(style class href src alt title colspan rowspan target id)
+
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
 
+    # config.active_record.schema_format = :sql
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
