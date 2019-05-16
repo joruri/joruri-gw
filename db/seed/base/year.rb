c1caef5c4721c37b59a7893c0506f360c6f12969
tao-k <tao@sitebridge.co.jp>
2019-05-16 14:12:03 +0900

    update demo fyear data

--- db/seed/base/year.rb
+++ db/seed/base/year.rb
@@ -18,6 +18,7 @@ create_year_marks('明治', 'M', '1868-09-08 00:00:00')
 create_year_marks('大正', 'T', '1912-07-30 00:00:00')
 create_year_marks('昭和', 'S', '1926-12-25 00:00:00')
 create_year_marks('平成', 'H', '1989-01-08 00:00:00')
+create_year_marks('令和', 'R', '2019-05-01 00:00:00')
 
 
 this_year = Date.today.month >= 4 ? Date.today.year.to_i : Date.today.prev_year.year.to_i
